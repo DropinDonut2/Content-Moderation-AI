615a02e0d6456a040d59e053a1d817d978e1ffe0
DropinDonut2 <livandojavier@gmail.com>
2026-02-09 11:55:04 +0800

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -9,6 +9,20 @@ const api = axios.create({
     }
 });
 
+// Add a request interceptor to attach the auth token
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('auth_token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 // ============ MODERATION ============
 
 export const moderateContent = async (data) => {
